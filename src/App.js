291eddb1fcc82afa54a33c9b20f0061e815dfeec
SYJoe <sungyeon52@gmail.com>
2023-06-06 05:09:44 +0900

    feat: Modify css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import logo from './aolda_logo.png';
+import logo from './images/aolda_logo.png';
+import scroll from './images/scroll.png';
 import './App.css';
 
 function App() {
@@ -27,6 +28,11 @@ function App() {
     setArgs(event.target.value);
   };
 
+  const handleScroll = () => {
+    const targetElement = document.getElementById('body');
+    targetElement.scrollIntoView({ behavior: 'smooth' });
+  };
+
   const handleButton = () => {
     fetch('http://localhost:8000/emit', {
       method: 'POST',
@@ -49,15 +55,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          {"Aolda"}
-        </h1>
-        <h5>
-          {"2023-1 파란학기제 아올다 팀입니다!"}
-        </h5>
+        <p className='font-title'>
+          {"Aolda-FlexiContract"}
+        </p>
+        <p className='font-subtitle'>
+          {"Solidity 외의 언어로 작성된 Smart Contract를 EVM 기반 체인에 적용하기 위한 L2 솔루션"}
+        </p>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          아올다 팀을 소개하는 각종 링크입니다 :)
+        <p className='font-link'>
+          아올다팀의 FlexiContract을 소개하는 링크입니다 :)
         </p>
         <a
           className="App-link"
@@ -75,15 +81,20 @@ function App() {
         >
           Velog
         </a>
+        <div onClick={handleScroll} className='scroll-button'>
+          <img src={scroll} alt='scroll' className='scroll-img' />
+        </div>
       </header>
 
-      <div className='container'>
+      <div className='App-container' id='body'>
+
+        <p className='font-1'>
+          callAolda에 필요한 데이터를 입력해주세요!
+        </p>
+
         <div>
-          <p>
-            callAolda에 필요한 데이터를 입력해주세요!
-          </p>
-          <div>
-            <span>
+          <div className='container-input'>
+            <span className='font-inputtitle'>
               fileHash :
             </span>
             <input
@@ -92,8 +103,8 @@ function App() {
               onChange={handleFileHash}
             />
           </div>
-          <div>
-            <span>
+          <div className='container-input'>
+            <span className='font-inputtitle'>
               functionName :
             </span>
             <input
@@ -102,9 +113,9 @@ function App() {
               onChange={handleFunctionName}
             />
           </div>
-          <div>
-            <span>
-              fileHash :
+          <div className='container-input'>
+            <span className='font-inputtitle'>
+              args :
             </span>
             <input
               type="text"
@@ -112,11 +123,12 @@ function App() {
               onChange={handleArgs}
             />
           </div>
-          <button onClick={handleButton}>callAolda</button>
         </div>
 
+        <button className='button-emit' onClick={handleButton}>callAolda</button>
+
         <div>
-          <p>Aolda를 통해 나온 결과 : {result}</p>
+          <p className='font-inputtitle'>Aolda를 통해 나온 결과 : {result}</p>
         </div>
       </div>
     </div>
