0954d2d282c110c40571b23025c6822d44e1c820
SYJoe <sungyeon52@gmail.com>
2023-06-08 02:48:03 +0900

    Add Result Transaction Section

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function Tooltip({ children, tooltip }) {
   };
 
   const renderTooltipContent = () => {
-    const lines = tooltip.split('\n'); // 입력된 값을 줄바꿈을 기준으로 나눔
+    const lines = tooltip.split('\n');
 
     return lines.map((line, index) => (
       <React.Fragment key={index}>
@@ -39,18 +39,16 @@ function Tooltip({ children, tooltip }) {
   );
 }
 
-
 function App() {
   const [fileHash, setFileHash] = useState('');
   const [functionName, setFunctionName] = useState('');
-  const [args, setArgs] = useState([]);
-  const [result, setResult] = useState('');
+  const [args, setArgs] = useState('');
   const [activeTab, setActiveTab] = useState(0);
   const [selectedFile, setSelectedFile] = useState(null);
   const [fileTx, setFileTx] = useState('');
   const [resultTx, setResultTx] = useState('');
 
-  
+
   const jsonData = {
     fileHash: fileHash,
     functionName: functionName,
@@ -69,13 +67,10 @@ function App() {
     setFunctionName(event.target.value);
   };
 
-  // const handleArgs = (event) => {
-  //   setArgs(event.target.value);
-  // };
   const handleArgs = (event) => {
-    setArgs(event.target.value.split('\n')); // 줄바꿈을 기준으로 문자열을 배열로 분할하여 설정
+    setArgs(event.target.value);
   };
-  
+
   const handleScrollDown = () => {
     const targetElement = document.getElementById('body');
     targetElement.scrollIntoView({ behavior: 'smooth' });
@@ -111,6 +106,7 @@ function App() {
 
   const handleButton = () => {
     console.log(jsonData);
+    jsonData.args = args.split(' ');
     fetch('http://localhost:8000/emit', {
       method: 'POST',
       headers: {
@@ -121,14 +117,15 @@ function App() {
       .then(response => response.text())
       .then(data => {
         console.log('POST 요청 결과:', data);
-        setResult(data);
+        setResultTx(data);
       })
       .catch(error => {
         console.error('POST 요청 중 오류 발생:', error);
       });
   };
 
-  const parsedData = fileTx ? JSON.parse(fileTx) : null;
+  const parsedFileTx = fileTx ? JSON.parse(fileTx) : null;
+  const parsedResultTx = resultTx ? JSON.parse(resultTx) : null;
 
   return (
     <div className="App">
@@ -163,10 +160,6 @@ function App() {
       </header>
 
       <div className="App-container" id="body">
-        <div onClick={handleScrollUp} className="scrollup-button">
-          <img src={scroll} alt="scroll" className="scroll-img" />
-        </div>
-
         <div className="tabs">
           <p
             style={{ color: activeTab === 0 ? '#61dafb' : 'black' }}
@@ -199,22 +192,22 @@ function App() {
 
               <div className={`transaction-section ${fileTx ? 'visible' : ''}`}>
                 <p className="font-inputtitle">Transaction</p>
-                {parsedData && (
+                {parsedFileTx && (
                   <div>
-                    <Tooltip tooltip={"Type은 트랜잭션의 종류를 나타내는 값으로 0~4 중 하나입니다.\n 0: 파일 생성\n 1: EVM에서 AOLDA를 호출한 기록\n 2: USER가 API를 사용해 직접 Aolda Node를 호출한 기록\n 3: type1과 type2에 대한 결과값\n 4: 블록 채굴에 대한 트랜잭션"}><strong>Type: </strong>{parsedData.header.type}</Tooltip>
-                    <Tooltip tooltip={"Hash는 트랜잭션 body에 대한 해쉬값입니다."}><strong>Hash:</strong> {parsedData.header.hash}</Tooltip>
-                    <Tooltip tooltip={"Block Number는 local blockchain의 block 개수입니다."}><strong>Block Number: </strong>{parsedData.header.blockNumber}</Tooltip>
-                    <Tooltip tooltip={"Transaction Index은 해당 Transaction의 순서입니다"}><strong>Transaction Index: </strong>{parsedData.header.transactionIndex}</Tooltip>
-                    <Tooltip tooltip={"From은 트랜잭션을 생성한 Node입니다."}><strong>From: </strong>{parsedData.header.from}</Tooltip>
-                    <Tooltip tooltip={"Nonce는 해당 트랜잭션을 채굴할 때 쓰이는 해쉬값과 연관된 값입니다."}><strong>Nonce: </strong>{parsedData.header.nonce}</Tooltip>
-                    <Tooltip tooltip={"Signature은 트랜잭션 유효성 검증을 위한 전자 서명입니다."}><strong>Signature R: </strong>{parsedData.header.signature.R}</Tooltip>
-                    <Tooltip tooltip={"Signature은 트랜잭션 유효성 검증을 위한 전자 서명입니다."}><strong>Signature S: </strong>{parsedData.header.signature.S}</Tooltip>
-                    <Tooltip tooltip={"Signature은 트랜잭션 유효성 검증을 위한 전자 서명입니다."}><strong>Signature V: </strong>{parsedData.header.signature.V}</Tooltip>
-                    <Tooltip tooltip={"Timestamp는 트랜잭션이 생성된 시간입니다."}><strong>Timestamp: </strong>{parsedData.header.timeStamp}</Tooltip>
-                    <Tooltip tooltip={"File Hash는 파일 공유 분산 네트워크인 IPFS에서 반환된 fileHash입니다.\ntype0 : 파일을 IPFS 네트워크에 저장하고 반환된 Hash값입니다.\ntype1, type2, type3 : 호출할 때 입력된 인자 중 fileHash에 해당하는 값입니다.\ntype4 : null값입니다."}><strong>File Hash: </strong>{parsedData.body.fileHash}</Tooltip>
-                    <Tooltip tooltip={"Function Name는 인자값으로 type1, type2, type3에서만 사용됩니다.\ntype0, type4 : null값입니다.\ntype1, type2, type3 : 호출할 때 입력된 인자 중 functionName에 해당하는 값입니다."}><strong>Function Name: </strong>{parsedData.body.functionName}</Tooltip>
-                    <Tooltip tooltip={"Arguments는 인자값으로 type1, type2, type3에서만 사용됩니다.\ntype0, type4 : null값입니다.\ntype1, type2, type3 : 호출할 때 입력된 인자 중 arguments에 해당하는 값입니다. \n인자들은 모두 string으로 들어와야 합니다."}><strong>Arguments: </strong>{parsedData.body.arguments}</Tooltip>
-                    <Tooltip tooltip={"Result는 결과값입니다.\ntype0, type1, type2 : null값입니다.\ntype3 : 함수 호출에 대한 결과값입니다.\ntype4 : 보상으로 지급되는 토큰의 수입니다."}><strong>Result: </strong>{parsedData.body.result}</Tooltip>
+                    <Tooltip tooltip={"Type은 트랜잭션의 종류를 나타내는 값으로 0~4 중 하나입니다.\n 0: 파일 생성\n 1: EVM에서 AOLDA를 호출한 기록\n 2: USER가 API를 사용해 직접 Aolda Node를 호출한 기록\n 3: type1과 type2에 대한 결과값\n 4: 블록 채굴에 대한 트랜잭션"}><strong>Type: </strong>{parsedFileTx.header.type}</Tooltip>
+                    <Tooltip tooltip={"Hash는 트랜잭션 body에 대한 해쉬값입니다."}><strong>Hash:</strong> {parsedFileTx.header.hash}</Tooltip>
+                    <Tooltip tooltip={"Block Number는 local blockchain의 block 개수입니다."}><strong>Block Number: </strong>{parsedFileTx.header.blockNumber}</Tooltip>
+                    <Tooltip tooltip={"Transaction Index은 해당 Transaction의 순서입니다"}><strong>Transaction Index: </strong>{parsedFileTx.header.transactionIndex}</Tooltip>
+                    <Tooltip tooltip={"From은 트랜잭션을 생성한 Node입니다."}><strong>From: </strong>{parsedFileTx.header.from}</Tooltip>
+                    <Tooltip tooltip={"Nonce는 해당 트랜잭션을 채굴할 때 쓰이는 해쉬값과 연관된 값입니다."}><strong>Nonce: </strong>{parsedFileTx.header.nonce}</Tooltip>
+                    <Tooltip tooltip={"Signature은 트랜잭션 유효성 검증을 위한 전자 서명입니다."}><strong>Signature R: </strong>{parsedFileTx.header.signature.R}</Tooltip>
+                    <Tooltip tooltip={"Signature은 트랜잭션 유효성 검증을 위한 전자 서명입니다."}><strong>Signature S: </strong>{parsedFileTx.header.signature.S}</Tooltip>
+                    <Tooltip tooltip={"Signature은 트랜잭션 유효성 검증을 위한 전자 서명입니다."}><strong>Signature V: </strong>{parsedFileTx.header.signature.V}</Tooltip>
+                    <Tooltip tooltip={"Timestamp는 트랜잭션이 생성된 시간입니다."}><strong>Timestamp: </strong>{parsedFileTx.header.timeStampe}</Tooltip>
+                    <Tooltip tooltip={"File Hash는 파일 공유 분산 네트워크인 IPFS에서 반환된 fileHash입니다.\ntype0 : 파일을 IPFS 네트워크에 저장하고 반환된 Hash값입니다.\ntype1, type2, type3 : 호출할 때 입력된 인자 중 fileHash에 해당하는 값입니다.\ntype4 : null값입니다."}><strong>File Hash: </strong>{parsedFileTx.body.fileHash}</Tooltip>
+                    <Tooltip tooltip={"Function Name는 인자값으로 type1, type2, type3에서만 사용됩니다.\ntype0, type4 : null값입니다.\ntype1, type2, type3 : 호출할 때 입력된 인자 중 functionName에 해당하는 값입니다."}><strong>Function Name: </strong>{parsedFileTx.body.functionName}</Tooltip>
+                    <Tooltip tooltip={"Arguments는 인자값으로 type1, type2, type3에서만 사용됩니다.\ntype0, type4 : null값입니다.\ntype1, type2, type3 : 호출할 때 입력된 인자 중 arguments에 해당하는 값입니다. \n인자들은 모두 string으로 들어와야 합니다."}><strong>Arguments: </strong>{parsedFileTx.body.arguments}</Tooltip>
+                    <Tooltip tooltip={"Result는 결과값입니다.\ntype0, type1, type2 : null값입니다.\ntype3 : 함수 호출에 대한 결과값입니다.\ntype4 : 보상으로 지급되는 토큰의 수입니다."}><strong>Result: </strong>{parsedFileTx.body.result}</Tooltip>
                   </div>
                 )}
               </div>
@@ -259,10 +252,27 @@ function App() {
                 callAolda
               </button>
 
-              <div>
-                <p className="font-inputtitle">
-                  Aolda를 통해 나온 결과 : {result}
-                </p>
+              <div className={`transaction-section ${resultTx ? 'visible' : ''}`}>
+                {parsedResultTx && (
+                  <div>
+                    <p className="font-inSection">Result : {parsedResultTx.body.result}</p>
+                    <strong className="font-inSection">Transaction</strong>
+                    <Tooltip tooltip={"Type은 트랜잭션의 종류를 나타내는 값으로 0~4 중 하나입니다.\n 0: 파일 생성\n 1: EVM에서 AOLDA를 호출한 기록\n 2: USER가 API를 사용해 직접 Aolda Node를 호출한 기록\n 3: type1과 type2에 대한 결과값\n 4: 블록 채굴에 대한 트랜잭션"}><strong>Type: </strong>{parsedResultTx.header.type}</Tooltip>
+                    <Tooltip tooltip={"Hash는 트랜잭션 body에 대한 해쉬값입니다."}><strong>Hash:</strong> {parsedResultTx.header.hash}</Tooltip>
+                    <Tooltip tooltip={"Block Number는 local blockchain의 block 개수입니다."}><strong>Block Number: </strong>{parsedResultTx.header.blockNumber}</Tooltip>
+                    <Tooltip tooltip={"Transaction Index은 해당 Transaction의 순서입니다"}><strong>Transaction Index: </strong>{parsedResultTx.header.transactionIndex}</Tooltip>
+                    <Tooltip tooltip={"From은 트랜잭션을 생성한 Node입니다."}><strong>From: </strong>{parsedResultTx.header.from}</Tooltip>
+                    <Tooltip tooltip={"Nonce는 해당 트랜잭션을 채굴할 때 쓰이는 해쉬값과 연관된 값입니다."}><strong>Nonce: </strong>{parsedResultTx.header.nonce}</Tooltip>
+                    <Tooltip tooltip={"Signature은 트랜잭션 유효성 검증을 위한 전자 서명입니다."}><strong>Signature R: </strong>{parsedResultTx.header.signature.R}</Tooltip>
+                    <Tooltip tooltip={"Signature은 트랜잭션 유효성 검증을 위한 전자 서명입니다."}><strong>Signature S: </strong>{parsedResultTx.header.signature.S}</Tooltip>
+                    <Tooltip tooltip={"Signature은 트랜잭션 유효성 검증을 위한 전자 서명입니다."}><strong>Signature V: </strong>{parsedResultTx.header.signature.V}</Tooltip>
+                    <Tooltip tooltip={"Timestamp는 트랜잭션이 생성된 시간입니다."}><strong>Timestamp: </strong>{parsedResultTx.header.timeStampe}</Tooltip>
+                    <Tooltip tooltip={"File Hash는 파일 공유 분산 네트워크인 IPFS에서 반환된 fileHash입니다.\ntype0 : 파일을 IPFS 네트워크에 저장하고 반환된 Hash값입니다.\ntype1, type2, type3 : 호출할 때 입력된 인자 중 fileHash에 해당하는 값입니다.\ntype4 : null값입니다."}><strong>File Hash: </strong>{parsedResultTx.body.fileHash}</Tooltip>
+                    <Tooltip tooltip={"Function Name는 인자값으로 type1, type2, type3에서만 사용됩니다.\ntype0, type4 : null값입니다.\ntype1, type2, type3 : 호출할 때 입력된 인자 중 functionName에 해당하는 값입니다."}><strong>Function Name: </strong>{parsedResultTx.body.functionName}</Tooltip>
+                    <Tooltip tooltip={"Arguments는 인자값으로 type1, type2, type3에서만 사용됩니다.\ntype0, type4 : null값입니다.\ntype1, type2, type3 : 호출할 때 입력된 인자 중 arguments에 해당하는 값입니다. \n인자들은 모두 string으로 들어와야 합니다."}><strong>Arguments: </strong>{parsedResultTx.body.arguments}</Tooltip>
+                    <Tooltip tooltip={"Result는 결과값입니다.\ntype0, type1, type2 : null값입니다.\ntype3 : 함수 호출에 대한 결과값입니다.\ntype4 : 보상으로 지급되는 토큰의 수입니다."}><strong>Result: </strong>{parsedResultTx.body.result}</Tooltip>
+                  </div>
+                )}
               </div>
             </div>
           )}
